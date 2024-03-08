5747cb329ae69327f8d641650c3d5caa6c7e3022
ruanm <247987314@qq.com>
2024-03-08 14:34:41 +0800

    Write pages and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Navbar from './sections/Navbar'
+import Wrapper from './sections/Wrapper'
+import Footer from './sections/Footer'
+import Background from './components/Background'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import './scss/index.scss'
+import Search from './pages/Search'
+import Pokemon from './pages/Pokemon'
+import MyList from './pages/MyList'
+import Compare from './pages/Compare'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='main-container'>
+      <Background />
+      
+      <BrowserRouter>
+        <div className='app'>
+          <Navbar />
+          <Routes>
+            <Route element={<Search />} path='/search'></Route>
+            <Route element={<MyList />} path='/list'></Route>
+            <Route element={<About />} path='/about'></Route>
+            <Route element={<Compare />} path='/compare'></Route>
+            <Route element={<Pokemon />} path='/pokemon/:id'></Route>
+            <Route element={<Navigate to='/pokemon/1' />} path="*"></Route>
+          </Routes>
+          {/* <Wrapper /> */}
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
