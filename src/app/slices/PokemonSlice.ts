1b142aad08fc81687e1530df546454caf9f67d08
ruanm <247987314@qq.com>
2024-03-11 14:59:43 +0800

    Write pokemon/initialData Api

--- src/app/slices/PokemonSlice.ts
+++ src/app/slices/PokemonSlice.ts
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { PokemonTypeInitialState } from "../../utils/types";
+import { getInitialPokemonData } from "../reducers/getInitialPokemonData";
 
-const initialState: PokemonTypeInitialState = {};
+const initialState: PokemonTypeInitialState = {
+  allPokemon: undefined
+};
 
 export const PokemonSlice = createSlice({
   name: "pokemon",
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getInitialPokemonData.fulfilled, (state, action) => {
+      state.allPokemon = action.payload;
+    });
+  }
 });
 
 export const {} = PokemonSlice.actions;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
 
 const Search = () => {
+  const dispatch = useAppDispatch();
+  const { allPokemon } = useAppSelector( ({pokemon}) => pokemon);
+
+  useEffect(()=>{
+    dispatch(getInitialPokemonData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if(allPokemon){
+      const copyPokemons = [...allPokemon];
+      const randomPokemonsId = copyPokemons.sort(()=>Math.random()-Math.random()).slice(0, 20);
+    }
+  }, [allPokemon]);
+
   return (
     <div>Search</div>
   )
